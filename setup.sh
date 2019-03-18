ccf9b370e941c5142f0cc4666f7f0a8f20d50318
Tyler Estes <estysdesu@gmail.com>
2019-03-18 20:40:54 +0900

    added jupyter-lab

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Shell command to start docker for project
-docker run --rm -p 8888:8888 -v "$PWD":/home/jovyan/work estysdesu/jupyter-data-science
+docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work estysdesu/jupyter-data-science
