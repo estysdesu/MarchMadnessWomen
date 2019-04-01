bd76c4943b3dae3e423a1b502699950dd49ad9b5
Tyler Estes <estysdesu@gmail.com>
2019-04-01 19:44:30 +0900

    refactor code

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# open web browser
+open /Applications/Safari.app
+
 # Shell command to start docker for project
 docker run --rm -it -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work estysdesu/jupyter-datasci
+
